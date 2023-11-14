72078499fc7b27979bbd82066bcdf133a9823f48
Pierre Ledeuil <pierre.ledeuil@epsi.fr>
2023-11-14 16:36:07 +0100

    Exercice 7- Fichiers

---
+++ ex7.py
@@ -0,0 +1,16 @@
+
+#On ouvre le fichier "ex7_input.txt", contenant un Lorem Ipsum de 100 mots.
+with open('ex7_input.txt', 'r') as input:
+    #On lit le contenu du fichier.
+    contenu = input.read()
+
+#On sépare les mots.
+mots = contenu.split()
+
+#On compte les mots.
+nb_mots = len(mots)
+
+#On ouvre le fichier "ex7_output.txt".
+with open('ex7_output.txt', 'w') as output:
+    #On écrit le nombre de mots dans le fichier.
+    output.write(str(nb_mots))
